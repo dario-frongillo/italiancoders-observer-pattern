0f610d0c1b7d56843d403acddcc8b96ff99bd6d4
d.frongillo <d.frongillo@list-group.com>
2018-05-08 13:54:19 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ObservableMatch match = new ObservableMatch();
+        Observer observer1 = new ObserverMatch("0");
+        Observer observer2 = new ObserverMatch("1");
+
+        match.addObserver(observer1);
+        match.addObserver(observer2);
+
+        match.setMatchScore("1-0");
+
+        match.removeObserver(observer2);
+        match.setMatchScore("2-0");
+
+    }
+}
